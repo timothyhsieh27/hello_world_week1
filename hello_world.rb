a44ee34fe6798bec5823798e7d33cd4eb6086d03
Tim <timothyhsieh27@gmail.com>
2016-12-09 18:35:29 -0500

    Restored name variable. Added greet user function. Function provides different greetings for different users.

--- hello_world.rb
+++ hello_world.rb
@@ -1,10 +1,19 @@
 def ask_for_name
   puts 'What is your name?'
-  puts "Hello, #{gets.chomp}, nice to meet you!"
+end
+
+def greet_user
+  name = gets.chomp
+  if name.downcase == 'darkness'
+    puts 'Hello Darkness, my old friend.'
+  else
+    puts "Hello, #{name}, nice to meet you!"
+  end
 end
 
 def main
   ask_for_name
+  greet_user
 end
 
 main if __FILE__ == $PROGRAM_NAME
